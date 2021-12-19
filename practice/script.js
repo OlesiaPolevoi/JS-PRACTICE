f581f555242696364acfb8337b8a5d3c731c383a
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-19 14:51:06 -0500

    lesson03 lecture practice

--- practice/script.js
+++ practice/script.js
@@ -1,56 +1,83 @@
 "use strict";
-// console.log("hi");
+// // console.log("hi");
+//NOTE Exercise
+// let num = 266219;
+// num = num.toString().split("");
 
-let num = 266219;
-num = num.toString().split("");
+// // num = num.toNumber();
+// // console.log(num);
+// let numArray = [];
 
-// num = num.toNumber();
-// console.log(num);
-let numArray = [];
-
-for (let i = 0; i < num.length; i++) {
-  numArray.push(parseInt(num[i]));
-}
-// console.log(numArray);
-
-let resul2 = 1; //1
-numArray.forEach((element) => {
-  resul2 = resul2 * element;
-});
-// console.log(resul2);
-const result3 = resul2 ** 3;
-let resul4 = result3.toString();
-// console.log(resul4[0] + resul4[1]);
-
-const arr = [1, 4, 3, 8, 8, 0, 99];
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i] + 2);
+// for (let i = 0; i < num.length; i++) {
+//   numArray.push(parseInt(num[i]));
 // }
+// // console.log(numArray);
 
-const nikitaForEach = (arr, callback) => {
-  for (let i = 0; i < arr.length; i++) {
-    callback(arr[i]);
-  }
-};
+// let resul2 = 1; //1
+// numArray.forEach((element) => {
+//   resul2 = resul2 * element;
+// });
+// // console.log(resul2);
+// const result3 = resul2 ** 3;
+// let resul4 = result3.toString();
+// // console.log(resul4[0] + resul4[1]);
 
-const cb = (element) => {
-  console.log(element + 2);
-};
+// const arr = [1, 4, 3, 8, 8, 0, 99];
 
-// nikitaForEach(arr, cb);
+// // for (let i = 0; i < arr.length; i++) {
+// //   console.log(arr[i] + 2);
+// // }
 
-arr.forEach((element) => {
-  console.log(element + 2);
-});
+// const nikitaForEach = (arr, callback) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     callback(arr[i]);
+//   }
+// };
 
-console.log(arr.toString());
+// const cb = (element) => {
+//   console.log(element + 2);
+// };
 
-// const func = (variable) => {};
+// // nikitaForEach(arr, cb);
 
-// 1) Создать переменную num со значением 266219 (тип данных число)
-// 2) Вывести в консоль произведение (умножение) цифр этого числа
-// Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) и перемножить их. Правильно использовать цикл или методы перебора.
-// 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+// arr.forEach((element) => {
+//   console.log(element + 2);
+// });
 
-// 4) Вывести в консоль первые 2 цифры полученного числа
+// console.log(arr.toString());
+
+// // const func = (variable) => {};
+
+// // 1) Создать переменную num со значением 266219 (тип данных число)
+// // 2) Вывести в консоль произведение (умножение) цифр этого числа
+// // Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) и перемножить их. Правильно использовать цикл или методы перебора.
+// // 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+
+// // 4) Вывести в консоль первые 2 цифры полученного числа
+
+////NOTE Notes
+// console.log(true + false);
+// console.log(+true);
+// console.log(+false);
+// console.log(12 / "6");
+
+// console.log("number" + 15 + 3);
+// console.log(15 + 3 + "number");
+
+// console.log([1] > null);
+// console.log("foo" + +"bar");
+
+// console.log("true" == true);
+// console.log(!!"true" == true);
+// console.log(false == "false");
+
+// console.log(null == "");
+// console.log(!!"false" == !!"true");
+// console.log([‘x’] == ‘x’);
+// console.log([] + null + 1);
+// console.log(0 || "0" && {});
+// console.log([1,2,3] == [1,2,3];
+// console.log({}+[]+{}+[1]);
+// console.log(!+[]+[]+![]);
+// console.log(new Date(0) - 0);
+// console.log(new Date(0) + 0);
