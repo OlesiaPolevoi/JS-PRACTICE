bca527c833406a51dd64b42772f25d761c3931fc
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-22 23:27:28 -0500

    practice commit

--- codewars/script.js
+++ codewars/script.js
@@ -1,1 +1,20 @@
 "use strict";
+// //NOTE
+// // Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// function squareSum(numbers) {
+//   let sum = 0;
+//   numbers.forEach(function (num) {
+//     sum += num ** 2;
+//   });
+//   return sum;
+// }
+// console.log(squareSum([1, 2]));
+
+// //OR with .reduce
+// // function squareSum(numbers){
+// //   return numbers.reduce(function(sum, n){
+// //     return (n*n) + sum;
+// //   }, 0)
+// // }
+
+// NOTE
